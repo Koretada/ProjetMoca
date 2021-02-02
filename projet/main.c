1fc8f6fcc1f58f0a19cbb5624fa26d4675ef6b1d
Koretada <koretadaa@gmail.com>
2021-02-02 22:13:23 +0100

    Etape 1
    Création d'un Makefile, utilisation des concepts modulaires.

---
+++ projet/main.c
@@ -0,0 +1,34 @@
+#include "structure.h"
+#include "dico.h"
+#include "maillon.h"
+#include "word.h"
+
+#define maxSizeWord 25
+
+int main(int argc, char* argv[])
+{
+  FILE* f;
+  if(argc == 2){
+    f = fopen(argv[1], "r");
+    if(f==NULL){
+      printf("Erreur dans l'ouverture du fichier\n");
+      exit(0);
+      }
+  }else {
+    printf("Utilisation : ./main <FichieraOuvrir>\n");
+    exit(0);
+  }
+  unsigned int* line = (unsigned int*) malloc(sizeof(int));
+  unsigned int* colonne = (unsigned int*) malloc(sizeof(int));
+  char* word = (char*) malloc(sizeof(char)*maxSizeWord);
+  dico* dictionary = (dico*) malloc(sizeof(dico));
+  if(f!=NULL){
+    while(!feof(f)) {
+      word = next_word(f,line,colonne); 
+      addToDico(dictionary,word,line,colonne); 
+    }
+  displayDico(dictionary);
+  fclose(f);
+  }
+  return 0;
+}
