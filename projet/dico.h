b968863a9da2cf42bccfc0297f7d00b05f9b6f71
Koretada <koretadaa@gmail.com>
2021-02-10 14:08:06 +0100

    Suppressions des anciens fichiers

--- projet/dico.h
+++
@@ -1,6 +0,0 @@
-#ifndef DICO_H
-#define DICO_H
-void insertDico(dico* dictionary, mot_t* linkWord);
-void addToDico(dico* dictionary, char* word, unsigned int* line, unsigned int* colonne);
-void displayDico(dico* dictionary);
-#endif
